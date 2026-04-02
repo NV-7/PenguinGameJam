f3060017379e2d19e448d1bd03d184d4caa2a139
matthewaasen <matthewaasen05@gmail.com>
2026-04-01 20:27:47 -0500

    added enemy health / health bar

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -11,17 +11,42 @@ public class EnemyController : MonoBehaviour
 
     Transform player;
     public float moveSpeed;
+
+    public float health;
+    private float maxHealth = 100f;
+
+    public float healthBarWidth = 0.25f;
+    public Transform healthBar;
+
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 135));
         transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+
+        healthBar.localScale = new Vector3(health / 100f * healthBarWidth, healthBar.localScale.y, healthBar.localScale.z);
+    }
+
+
+//player must have rigidbody (dynamic) and circle collider
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            health =- 10f;
+        }
     }
 }
