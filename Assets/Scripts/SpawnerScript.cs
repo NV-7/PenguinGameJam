8e50b34ff3e7806e25e8393cd7a41cd0043e7959
NV-7 <naviboun@gmail.com>
2026-04-07 13:01:25 -0500

    Updated Spawner
    Spawner should now spawn enemies in a radius around the player, outside their Point of View. Also the pawn rate of enemies should increase as the time progresses

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -7,36 +7,40 @@ public class SpawnerScript : MonoBehaviour
     public int enenmyCounter = 0;
     public GameObject Enemy1;
     public float TimeOffset = 2f;
+    public Transform playerTransform;
+    public float spawnRadius = 10f;
+    public float spawnInterval = 2f; // Two second spawn interval
     private float TimePassed = 0f;
-    private float timeSinceStart = 0f;
+    private float timer = 0f; 
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeSinceStart += Time.deltaTime;
+       // Changess spawn rate as game continues, Max spawn rate is 0.2
+        float interval = Mathf.Max(0.3f, spawnInterval - (Time.timeSinceLevelLoad / 60f));
+
+        if (enenmyCounter < maxNumEnemy)
+        {
+            timer += Time.deltaTime;
+            if (timer >= interval)
+            {
+                spawnEnemy();
+                timer= 0f;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        TimePassed += Time.deltaTime;
-        
-
-        if (enenmyCounter < maxNumEnemy)
-        {
-
-            if(TimePassed >= TimeOffset)
-            {
-            spawnEnemy();
-            enenmyCounter++;
-            TimePassed = 0f;
-            }
-        }
+       
         
      
             
@@ -44,8 +48,19 @@ public class SpawnerScript : MonoBehaviour
 
     void spawnEnemy()       
     {
-        Instantiate(Enemy1, this.transform.position, Quaternion.identity);
+        // Vector2 inside a unit circle
+        Vector2 unitDirection = Random.insideUnitCircle.normalized;
 
+        Vector3 spawnPos = playerTransform.position + new Vector3(unitDirection.x, unitDirection.y, 0) * spawnRadius;
+
+        Instantiate(Enemy1, spawnPos, Quaternion.identity);
+        enenmyCounter++;
+
+    }
+
+    public void descrementEnemeyCount()
+    {
+        enenmyCounter--;
     }
 
 }
